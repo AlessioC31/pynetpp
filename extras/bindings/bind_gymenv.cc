553060b96027fef905cabae19c9eed67a20703e9
Alessio Cosenza <cosenzaalessio17@gmail.com>
2022-08-05 23:06:42 +0200

    fix: now python takes ownership of the observation

--- extras/bindings/bind_gymenv.cc
+++ extras/bindings/bind_gymenv.cc
@@ -9,7 +9,7 @@ void bind_gymenv(pybind11::module_ &m) {
     )
         .def("get_observation_space", &OpenGymEnv::get_observation_space, pybind11::return_value_policy::reference)
         .def("get_action_space", &OpenGymEnv::get_action_space, pybind11::return_value_policy::reference)
-        .def("get_observation", &OpenGymEnv::get_observation, pybind11::return_value_policy::reference)
+        .def("get_observation", &OpenGymEnv::get_observation, pybind11::return_value_policy::take_ownership)
         .def("execute_action", &OpenGymEnv::execute_action)
         .def("get_reward", &OpenGymEnv::get_reward)
         .def("get_game_over", &OpenGymEnv::get_game_over)
