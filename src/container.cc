6b8c92f58267bb4e0d755007b8d9cea3bab25b45
Alessio Cosenza <cosenzaalessio17@gmail.com>
2022-08-07 17:02:37 +0200

    upd: added range check for discrete container

--- src/container.cc
+++ src/container.cc
@@ -6,6 +6,10 @@ PynetppDiscreteContainer::PynetppDiscreteContainer(): space_n(0) {}
 PynetppDiscreteContainer::PynetppDiscreteContainer(uint32_t n): space_n(n) {}
 
 void PynetppDiscreteContainer::set_value(uint32_t value) {
+    if (value >= space_n) {
+        throw std::out_of_range("Value is out of range for this container.");
+    }
+
     inner_value = value;
 }
 
