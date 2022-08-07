8a345540f74df14d14355dce25484a574c15ac87
Alessio Cosenza <cosenzaalessio17@gmail.com>
2022-08-07 16:33:15 +0200

    upd: moved dtypes to header only

--- include/dtypes.h
+++ include/dtypes.h
@@ -16,19 +16,19 @@ enum PynetppDType {
 };
 
 template<typename T>
-PynetppDType get_dtype() {
+constexpr PynetppDType get_dtype() {
     // TODO: should throw?
     return FLOAT;
 }
 
-template <> PynetppDType get_dtype<int8_t> ();
-template <> PynetppDType get_dtype<int16_t> ();
-template <> PynetppDType get_dtype<int32_t> ();
-template <> PynetppDType get_dtype<int64_t> ();
-template <> PynetppDType get_dtype<uint8_t> ();
-template <> PynetppDType get_dtype<uint16_t> ();
-template <> PynetppDType get_dtype<uint32_t> ();
-template <> PynetppDType get_dtype<uint64_t> ();
-template <> PynetppDType get_dtype<float> ();
-template <> PynetppDType get_dtype<double> ();
+template <> constexpr PynetppDType get_dtype<int8_t> () { return INT8; }
+template <> constexpr PynetppDType get_dtype<int16_t> () { return INT16; }
+template <> constexpr PynetppDType get_dtype<int32_t> ()  { return INT32; }
+template <> constexpr PynetppDType get_dtype<int64_t> () { return INT64; }
+template <> constexpr PynetppDType get_dtype<uint8_t> () { return UINT8; }
+template <> constexpr PynetppDType get_dtype<uint16_t> () { return UINT16; }
+template <> constexpr PynetppDType get_dtype<uint32_t> () { return UINT32; }
+template <> constexpr PynetppDType get_dtype<uint64_t> () { return UINT64; }
+template <> constexpr PynetppDType get_dtype<float> () { return FLOAT; }
+template <> constexpr PynetppDType get_dtype<double> () { return DOUBLE; }
 #endif
