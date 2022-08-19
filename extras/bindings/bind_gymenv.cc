58d51e2b5f9814a1f9c576ed5a28d9a4c6865b5a
Alessio Cosenza <cosenzaalessio17@gmail.com>
2022-08-20 00:30:26 +0200

    upd: chagend policy in gymenv bindings

--- extras/bindings/bind_gymenv.cc
+++ extras/bindings/bind_gymenv.cc
@@ -1,15 +1,15 @@
+#include <gymenv.h>
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
-#include <gymenv.h>
 
-void bind_gymenv(pybind11::module_ &m) {
-    pybind11::class_<PynetppEnv>(
-        m,
-        "PynetppEnv"
-    )
-        .def("get_observation_space", &PynetppEnv::get_observation_space, pybind11::return_value_policy::reference)
-        .def("get_action_space", &PynetppEnv::get_action_space, pybind11::return_value_policy::reference)
-        .def("get_observation", &PynetppEnv::get_observation, pybind11::return_value_policy::take_ownership)
+void bind_gymenv(pybind11::module_& m) {
+    pybind11::class_<PynetppEnv>(m, "PynetppEnv")
+        .def("get_observation_space", &PynetppEnv::get_observation_space,
+             pybind11::return_value_policy::take_ownership)
+        .def("get_action_space", &PynetppEnv::get_action_space,
+             pybind11::return_value_policy::take_ownership)
+        .def("get_observation", &PynetppEnv::get_observation,
+             pybind11::return_value_policy::take_ownership)
         .def("execute_action", &PynetppEnv::execute_action)
         .def("get_reward", &PynetppEnv::get_reward)
         .def("get_game_over", &PynetppEnv::get_game_over)
